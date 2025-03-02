1a28d979c2b2e1128d2feb356a002d0b9f6bc895
soumyajeet17 <smvssut17@gmail.com>
2025-03-02 21:40:05 +0530

    Your commit message here

--- catkin_ws/src/arduino_node_test1/arduino_test2.py
+++ catkin_ws/src/arduino_node_test1/arduino_test2.py
@@ -3,11 +3,9 @@
 import rospy
 from std_msgs.msg import Float32
 
-# Global variables to store data from Arduino nodes
 data_arduino1 = 1.0
 data_arduino2 = 2.0
 
-# Callback for Arduino 1
 def callback_arduino1(msg):
     global data_arduino1
     data_arduino1 = msg.data
@@ -22,21 +20,20 @@ def callback_arduino2(msg):
 def publisher():
     rospy.init_node('arduino_comm', anonymous=True)
 
-    # Publisher for Arduino 1 and Arduino 2
     pub1 = rospy.Publisher('n1', Float32, queue_size=10)
     pub2 = rospy.Publisher('n2', Float32, queue_size=10)
 
-    # Subscriber for Arduino 1 and Arduino 2
     rospy.Subscriber('n1', Float32, callback_arduino1)
     rospy.Subscriber('n2', Float32, callback_arduino2)
 
-    rate = rospy.Rate(1)  # 1 Hz rate
+    rate = rospy.Rate(1)  
     while not rospy.is_shutdown():
-        # Publish data to both topics
+        #rospy.loginfo("Publishing data to Arduino 1: %f", data_arduino1)
+        #rospy.loginfo("Publishing data to Arduino 2: %f", data_arduino2)
+
         pub1.publish(data_arduino1)
         pub2.publish(data_arduino2)
 
-        # Sleep to maintain the loop rate
         rate.sleep()
 
 if __name__ == '__main__':
